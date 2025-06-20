63c81f8ac2dcba27947ca5c077573ad7353436e4
Alexander Smirnov <adsmirnov13@gmail.com>
2025-06-20 11:50:58 +0300

    changed fields of commit

--- main/src/main/java/gitactivity/main/model/Commit.java
+++ main/src/main/java/gitactivity/main/model/Commit.java
@@ -3,14 +3,13 @@ package gitactivity.main.model;
 import lombok.Data;
 
 import java.sql.Time;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 public class Commit {
     private int id;
-    private Time time;
-    private Date date;
-    private String branch;
+    private LocalDateTime date;
     private int changedLines;
     private String user;
     private String comment;
