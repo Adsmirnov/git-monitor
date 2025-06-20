64c681ccb4f0154877219062afb1b3c361679cf9
mileZ239 <misalazarev42@gmail.com>
2025-06-20 16:16:27 +0300

    added CommitParserService.java 2

---
+++ main/src/main/java/gitactivity/main/CommitParserService.java
@@ -0,0 +1,38 @@
+package gitactivity.main;
+
+import gitactivity.main.model.Commit;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CommitParserService {
+
+    @Autowired
+    GitApiService gitApiService;
+
+    public ArrayList<Commit> getParsedCommits() {
+        ArrayList<Commit> parsedCommits = new ArrayList<>();
+
+        ArrayList<String> allCommits = gitApiService.getProcessedData();
+        System.out.println("[COMMITS]");
+        for (int i = 0; i < allCommits.size(); i++) {
+            JSONArray currentRepoCommits = new JSONArray(allCommits.get(i));
+            for (int j = 0; j < currentRepoCommits.length(); j++) {
+                JSONObject currentCommit = new JSONObject(currentRepoCommits.get(j).toString());
+
+                Commit commit = new Commit();
+                commit.setId((String) currentCommit.get("id"));
+                commit.setUser((String) currentCommit.get("committer_name"));
+                commit.setComment((String) currentCommit.get("message"));
+                System.out.println("[COMMIT] " + commit);
+            }
+        }
+
+        return parsedCommits;
+    }
+
+}
