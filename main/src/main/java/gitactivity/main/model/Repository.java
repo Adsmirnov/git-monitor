a06ebee745abe4302c112f9a18e68d38d2e5e9ed
Alexander Smirnov <adsmirnov13@gmail.com>
2025-06-20 10:37:44 +0300

    added entities and repositories

--- main/src/main/java/gitactivity/main/model/Repository.java
+++ main/src/main/java/gitactivity/main/model/Repository.java
@@ -1,12 +1,22 @@
 package gitactivity.main.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.util.List;
 
+@Entity
+@Table(name = "repositories")
 @Data
 public class Repository {
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "url")
     private String url;
+
+    @Column(name = "sha")
     private String sha;
 }
--- main/src/main/java/gitactivity/main/model/User.java
+++ main/src/main/java/gitactivity/main/model/User.java
@@ -1,11 +1,17 @@
 package gitactivity.main.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.util.List;
 
+@Entity
+@Table(name = "users")
 @Data
 public class User {
+    @Column(name = "login")
     private String login;
 
 }
