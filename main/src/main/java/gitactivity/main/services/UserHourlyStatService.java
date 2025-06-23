7f9e3eb95db2d88bd35dcfcf92a449419b1d6ae8
mileZ239 <misalazarev42@gmail.com>
2025-06-23 15:30:28 +0300

    saveUserHourlyStat works now

--- main/src/main/java/gitactivity/main/services/UserHourlyStatService.java
+++ main/src/main/java/gitactivity/main/services/UserHourlyStatService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -25,14 +26,21 @@ public class UserHourlyStatService {
     public void saveUserHourlyStat() {  // Метод для создания и заполнения новой ячейки в базе данных
         userHourlyStatRepository.clearTable();
         UserHourlyStat stat = new UserHourlyStat();
-        ArrayList<Commit> commits = commitParserService.getParsedCommits(LocalDateTime.now().minusHours(5), LocalDateTime.now());
-        int sumOfChangedLines = 0;
-        for (Commit commit : commits) {
-            sumOfChangedLines+=commit.getChangedLines();
+        Map<String, ArrayList<Commit>> commits = commitParserService.getParsedCommits(LocalDateTime.now().minusHours(5), LocalDateTime.now());
+
+        ArrayList<Commit> commitsOfUser = commits.get("mileZ239");
+        if (commitsOfUser == null) {
+            return;
         }
 
+        int sumOfChangedLines = 0;
+        for (Commit c : commitsOfUser) {
+            sumOfChangedLines += c.getChangedLines();
+        }
+
+
         stat.setLogin("mileZ239");
-        stat.setCommits(commits.size());
+        stat.setCommits(commitsOfUser.size());
         stat.setLines(sumOfChangedLines);
         stat.setDate(LocalDateTime.now());
 
